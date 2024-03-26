b5c7f8f84ad1c0a4ca95e70f444d190577225d5a
Hemant Bisht <bishthemant169@gmail.com>
2024-03-26 08:20:06 +0530

    send message functionality

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,21 +1,17 @@
+import useGetMessages from "../../hooks/useGetMessages";
+import MessageSkeleton from "../skeletons/MessageSkeleton";
 import Message from "./Message";
 
 const Messages = () => {
-	return (
-		<div className='px-4 flex-1 overflow-auto'>
-			<Message />
-			<Message />
-			<Message />
-			<Message />
-			<Message />
-			<Message />
-			<Message />
-			<Message />
-			<Message />
-			<Message />
-			<Message />
-			<Message />
-		</div>
-	);
+  const { messages, loading } = useGetMessages();
+  console.log("messages: ", messages);
+  return (
+    <div className="px-4 flex-1 overflow-auto">
+      {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
+      {!loading && messages.length === 0 && (
+        <p className="text-center">Send a message to start the conversation</p>
+      )}
+    </div>
+  );
 };
 export default Messages;
