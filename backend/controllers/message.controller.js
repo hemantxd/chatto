25c61456b2aa5f0449ae6e7eb2ff1ce22d60b533
Hemant Bisht <bishthemant169@gmail.com>
2024-03-27 08:34:14 +0530

    done

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
+import { getReceiverSocketId, io } from "../socket/socket.js";
 
 export const sendMessage = async (req, res) => {
   try {
@@ -26,14 +27,21 @@ export const sendMessage = async (req, res) => {
       conversation.messages.push(newMessage._id);
     }
 
-    //socket.io functionality will go here
-
+    
     //await conversation.save()
     //await newMessage.save()
-
+    
     await Promise.all([conversation.save(), newMessage.save()]);
+   
+   
+    //socket.io functionality will go here
+const receiverSocketId = getReceiverSocketId(recieverId);
+if (receiverSocketId) {
+  // io.to(<socket_id>).emit() used to send events to specific client
+  io.to(receiverSocketId).emit("newMessage", newMessage);
+}
 
-    res.status(201).json(newMessage);
+res.status(201).json(newMessage);
   } catch (error) {
     console.log("error in send message controller", error.message);
     res.status(500).json({ error: "internal server error" });
@@ -53,7 +61,7 @@ export const getMessages = async (req, res) => {
 
     const messages = conversation.messages;
 
-    res.status(200).json(messages)
+    res.status(200).json(messages);
   } catch (error) {
     console.log("error in get message controller", error.message);
     res.status(500).json({ error: "internal server error" });
